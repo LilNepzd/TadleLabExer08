e31c6c2eafb1afd14a5654fa674f2487ff3ad510
Nepzd <60205478+Nepzd@users.noreply.github.com>
2020-02-26 10:24:51 +0800

    Lab Exercise 08
    Airline booking simulation

---
+++ Tadle08.js
@@ -0,0 +1,24 @@
+function input() {
+
+    var lastName = document.getElementById("LastName").value;
+    var firstName = document.getElementById("Gender").value;
+    var gender = document.getElementById("Gender").value;
+    var email = document.getElementById("Email").value;
+    var origin = document.getElementById("Origin").value;
+    var destination = document.getElementById("Destination").value;
+    var departure = document.getElementById("Departure").value;
+    var returndate = document.getElementById("Return").value;
+
+
+
+    if (!lastName || !firstName || !email || !origin || !destination || !departure || !returndate || gender.value == "Select") {
+        alert("Fill up all fields first.");
+    } else if (email.includes("@") == false) {
+        alert("Email must include @.");
+    } else if (departure >= returndate) {
+        alert("Make sure both date fields are filled up, and the return date is later than the departure date.");
+    } else {
+        document.getElementById("display").innerHTML = "Data Submitted Successfully.";
+    }
+
+}
